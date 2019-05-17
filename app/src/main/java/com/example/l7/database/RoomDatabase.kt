6ff168913fc7c9777e28d11551a56f7aae442749
karichalobato <00002517@uca.edu.sv>
2019-05-17 08:44:30 -0600

    commit

--- app/src/main/java/com/example/l7/database/RoomDatabase.kt
+++ app/src/main/java/com/example/l7/database/RoomDatabase.kt
@@ -1,13 +1,32 @@
 package com.example.l7.database
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.l7.database.entities.GithubRepo
-
+import java.security.AccessControlContext
 
 
 @Database(entities = [GithubRepo::class],version = 1,exportSchema = false)
 public abstract class RoomDatabase : RoomDatabase(){
+    abstract fun repoDao():GithubRepo
     companion object {
+        @Volatile
+        private var INSTANCE: RoomDatabase? =null
+
+        fun getInstance(Appcontext:Context):RoomDatabase{
+            val temp = INSTANCE
+
+            if(temp !=null){
+                return temp
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(Appcontext,RoomDatabase::class.java,"RepoDB")
+                    .build()
+
+                INSTANCE =instance
+                return instance
+            }
+        }
 
     }
 
