88c12042340332046b90838c2c8965bc89ce4468
karichalobato <00002517@uca.edu.sv>
2019-05-17 08:48:21 -0600

    commit

--- app/src/main/java/com/example/l7/activities/MainActivity.kt
+++ app/src/main/java/com/example/l7/activities/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.l7.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.AndroidViewModel
 import com.example.l7.R
+import com.example.l7.viewmodels.GithubRepoViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: GithubRepoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun bind(){
+        viewModel
     }
 }
